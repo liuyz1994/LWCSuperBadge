c2bcf992a6894499e66ede3575a4a9b92d9f8025
liuyz1994 <liuyz0823@gmail.com>
2022-11-19 17:01:00 +0100

    add file

---
+++ parsePR.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const readline = require('readline')
+
+async function extractTests(){
+
+    //by default we specify that all tests should run
+    let testsFile = __dirname+'/testsToRun.txt';
+    await fs.promises.writeFile(testsFile,'all');
+
+    const lines = readline.createInterface({
+        input: fs.createReadStream(__dirname+'/pr_body.txt'),
+        crlfDelay: Infinity
+    });
+
+    for await (const line of lines) {
+        //special delimeter for apex tests
+        if(line.includes('TestClass:[')){
+            let tests = line.substring(9,line.length-1);
+            await fs.promises.writeFile(testsFile,tests);
+            await fs.promises.appendFile(testsFile,'\n');
+        }
+    }
+}
+
+extractTests();
